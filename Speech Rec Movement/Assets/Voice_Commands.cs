c79b159556021ed8b1732be6f2daa9f2991060c2
Lawrence Gan <lbgan2000@berkeley.edu>
2020-04-15 17:16:18 -0700

    fixed something

--- Speech Rec Movement/Assets/Voice_Commands.cs
+++ Speech Rec Movement/Assets/Voice_Commands.cs
@@ -9,14 +9,14 @@ public class Voice_Commands : MonoBehaviour
 {
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
-    private int _attackCount = 0;
+
     private void Start()
     {
         actions.Add("left", Left);
         actions.Add("right", Right);
-        actions.Add("mid", Mid);
         actions.Add("attack", Attack);
         actions.Add("tack", Attack);
+        actions.Add("down", Down);
 
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
@@ -27,44 +27,25 @@ public class Voice_Commands : MonoBehaviour
     {
         Debug.Log(speech.text);
         actions[speech.text].Invoke();
-        transform.position = new Vector3(0, 0, 0);
     }
 
     private void Left()
     {
-        /**Go to the left*/
         transform.Translate(1, 0, 0);
-        //transform.position = new Vector3(5, 0, 0);
     }
 
     private void Right()
     {
-        /**Go to the right*/
         transform.Translate(-1, 0, 0);
-        //transform.position = new Vector3(-5, 0, 0);
-    }
-
-    private void Mid()
-    {
-        /**Go to the middle*/
-        transform.Translate(-1, 0, 0);
-        //transform.position = new Vector3(0, 0, 0);
     }
 
     private void Attack()
     {
-        /**This is just filler for us to add an attack*
-         * it alternates beteen making the thing go up or down
-         * */
-        if (_attackCount % 2 != 0)
-        {
-            transform.Translate(0, 1, 0);
-        }
-        else 
-        {
-            transform.Translate(0, -1, 0);
-        }
-        _attackCount += 1;
+        transform.Translate(0, 1, 0);
     }
 
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
 }
